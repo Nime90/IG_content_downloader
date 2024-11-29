c03d7c4e69de4e5ba0e2f8c1da815cc2be1d56ca
NIME90 <nicola.menale@lenus.io>
2024-11-29 14:13:40 +0100

    updating for google colab

---
+++ app_colab.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from utils.download_content import download_content
+import zipfile
+import os,shutil
+
+# Streamlit App Title
+st.title("Instagram Content Downloader")
+
+st.write("""
+This application allows you to download multiple videos or carousel items from Instagram.
+Enter the URLs and download all content as a single ZIP file.
+""")
+
+# User input for the video URL
+urls = st.text_area("Enter Instagram URLs (one per line):")
+
+# Button to download the content
+if st.button("Download"):
+    try:
+        #clean the old results if any
+        if os.path.exists('results'): shutil.rmtree('results')
+        
+        # Call the download_content function
+        url_list = [url.strip() for url in urls.splitlines() if url.strip()]
+        captions = []
+        additional_info_lists = []
+        for url in url_list:
+            file_names, caption, additional_info_list = download_content(url)
+            captions.append(caption)
+            additional_info_lists.append(additional_info_list)
+        
+        for i,url in enumerate(url_list):
+            st.write('Caption for',str(url.split('/'))[-2],': ',captions[i])
+            st.write('Additional info for',str(url.split('/'))[-2],': ',additional_info_lists[i])
+            
+            info_txt='Caption for '+str(url.split('/')[-2])+': '+str(captions[i])+'\n\nAdditional info for '+str(url.split('/')[-2])+': '+str(additional_info_lists[i])
+            with open('results/Additional_info_on_'+str(url.split('/')[-2])+".txt", "w") as file: file.write(info_txt)
+        
+        # Create a ZIP file to bundle all downloaded files
+        files_for_zip = [f for f in os.listdir('results') if os.path.isfile(os.path.join('results', f))]
+        zip_path = "results/downloaded_files.zip"
+        with zipfile.ZipFile(zip_path, 'w') as zipf:
+            for file_name in files_for_zip:
+                file_path = os.path.join("results", file_name)
+                zipf.write(file_path, arcname=file_name)
+        
+        #check amount of downloads:
+        if len(file_names) < 2: file_name_dwnld=file_names[0]
+        else: file_name_dwnld="downloaded_files.zip"
+        # Provide a single download button for the ZIP file
+        with open(zip_path, "rb") as zip_file:
+            st.download_button(
+                label="Click here to download all files",
+                data=zip_file,
+                file_name=file_name_dwnld,
+                mime="application/zip"
+            )
+        
+        st.success("File(s) correctly downloaded and zipped!")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
--- utils/download_content.py
+++ utils/download_content.py
@@ -1,61 +1,85 @@
-def download_content_v0(video_url='https://www.instagram.com/p/DC11wzFsGul/?hl=en'):
-    from selenium import webdriver
+def download_content_gc(video_url):
+    import google_colab_selenium as gs
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.common.by import By
-    import time, os, shutil
-
-
+    import time, os,requests
+    from bs4 import BeautifulSoup
     #create results folder if does not exists
-    res_folder=os.getcwd()+'/results'
-    if os.path.exists(res_folder): shutil.rmtree(res_folder)
+    res_folder='/content/results'
+    #if os.path.exists(res_folder): shutil.rmtree(res_folder)
     os.makedirs(res_folder, exist_ok=True)
+
+    #video_url= 'https://www.instagram.com/p/DChnh9yxQ0q/?hl=en&img_index=1'#'https://www.instagram.com/p/DC11wzFsGul/?hl=en'
     #initiate webdriver
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Optional: run in headless mode
-    prefs = {"download.default_directory" : res_folder}
-    chrome_options.add_experimental_option("prefs",prefs)
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--window-size=1920,1080")  # Set the window size
+    chrome_options.add_argument("--disable-infobars")  # Disable the infobars
+    chrome_options.add_argument("--disable-popup-blocking")  # Disable pop-ups
+    chrome_options.add_argument("--ignore-certificate-errors")  # Ignore certificate errors
+    chrome_options.add_argument("--incognito")  # Use Chrome in incognito mode
+    chrome_options.add_argument(f'--download-dir={res_folder}')  
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = gs.Chrome(options=chrome_options)
     driver.get('https://fastdl.app/en')
     driver.implicitly_wait(10) 
-    
+
     while True:
         try:
             input_element=driver.find_element(By.CSS_SELECTOR,'input')
             driver.implicitly_wait(10)
             input_element.click()
+
+            driver.implicitly_wait(10) 
+            input_element.send_keys(video_url)
+            driver.implicitly_wait(10) 
+            button = driver.find_element(By.CSS_SELECTOR, 'button.search-form__button[type="submit"]')
+            button.click()
+            driver.implicitly_wait(10)
+            time.sleep(3)
             break
         except:
             time.sleep(0.5)
             pass
-    driver.implicitly_wait(10) 
-    input_element.send_keys(video_url)
-    driver.implicitly_wait(10) 
-    button = driver.find_element(By.CSS_SELECTOR, 'button.search-form__button[type="submit"]')
-    # Click on the button
-    button.click()
-    driver.implicitly_wait(10)
-    time.sleep(3)
 
-    buttons = driver.find_elements(By.CSS_SELECTOR, "a.button.button--filled.button__download")
-    for i,button in enumerate(buttons):
-        driver.implicitly_wait(5) 
-        button.click()
-        latest_file='None'
-        while True:
-            time.sleep(2)
-            downloaded_files = os.listdir(res_folder)
-            try:
-                latest_file = max([os.path.join(res_folder, f) for f in downloaded_files], key=os.path.getctime)
-            except: pass
-            if latest_file[-4:] == '.mp4' or latest_file[-4:] == '.jpg':
+    while True:
+        try:
+            buttons = driver.find_elements(By.CSS_SELECTOR, "a.button.button--filled.button__download")
+            button_list=[]
+            for button in buttons:
+                driver.implicitly_wait(5) 
+                button_list.append(button.get_attribute('href'))
+            for i,url in enumerate(button_list):
+                if '.mp4' in url: extension='.mp4'
+                else: extension='.jpg'
+                r = requests.get(url)
+                results_directory=res_folder+'/'+video_url.split('/')[-2]+'_'+str(i)+extension
+                with open(results_directory, 'wb') as f:
+                    f.write(r.content)
+            Last_created_file=str(video_url.split('/')[-2]+'_'+str(i)+extension)
+            if Last_created_file in [f for f in os.listdir('results') if os.path.isfile(os.path.join('results', f))]:
                 break
-        new_file_name = video_url.split('/')[-2]+'_'+str(i)+latest_file[-4:]
-        shutil.move(latest_file, res_folder+'/'+new_file_name)
+        except:
+            time.sleep(0.5)
+            pass
+
+    #capturing additional_info
+    while True:
+        try:
+            additional_info=driver.find_elements(By.CLASS_NAME, "output-list__info")
+            additional_info_list = additional_info[0].text.split('\n')
+            if len(additional_info_list)>0:
+                break
+        except:
+            time.sleep(0.5)
+            pass
+    #get caption
+    caption = BeautifulSoup(requests.get(video_url).text, 'html.parser').find_all('meta', property="og:description")[0]['content']
 
     driver.quit()
-
-    return new_file_name
+    files = [f for f in os.listdir('results') if os.path.isfile(os.path.join('results', f))]
+    
+    return files, caption, additional_info_list
 
 
 def download_content(video_url):
