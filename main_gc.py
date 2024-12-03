6d90070a51444e5580add4db7e180557252e15ce
NIME90 <nicola.menale@lenus.io>
2024-12-03 10:52:36 +0100

    adding mp4 to spj

--- main_gc.py
+++ main_gc.py
@@ -1,5 +1,7 @@
 from utils.download_content import download_content_gc as download_content
 from utils.coach_ig_info import coach_ig_info_gc as coach_ig_info
+from utils.mp4_to_jpg import mp4_to_jpg
+
 import os, shutil
 
 #clean results
@@ -9,14 +11,19 @@ coach_url = 'https://www.instagram.com/monetzamora_/?hl=en'
 posts_info = coach_ig_info(coach_url.split('/')[-2])
 
 url_list=[l for l in posts_info.perma_link]
-url_list = url_list[:3]
+url_list = url_list[:1]
 print('urls captured')
 print(url_list)
 #download in results folder
 captions = []
 additional_info_lists = []
 for i,url in enumerate(url_list):
+    #get the name of the downloaded files
     file_names = download_content(url)
+    #if i download a video i will split it into frames
+    if 'mp4' in file_names[0]:
+      mp4_to_jpg(file_names[0])
+      os.remove('/content/results/'+str(file_names[0]))
     captions.append(posts_info.text[i])
     additional_info_lists.append(
       'video_views: '+str(posts_info.lifetime_video_views[i])+
